bb491a0e00322b8a5799b9a306b05a37fd2df11a
Fight Light Diamond <i.am.m.cuong@gmail.com>
2017-01-03 08:04:20 +0700

    0k

--- quickstart-master/app/app.component.ts
+++ quickstart-master/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}!!!</h1>
+  template: `<h1>Hello {{name}} - {{year}}!!!</h1>
             <my-tutorial></my-tutorial>
+            <img [src]="image" alt="">
+            <input type="text" [value]="welcomeText"> {{welcomeText}}
+            <h4 [style.color]="blueColor?'blue':'green'">You are victory</h4>
 `,
+
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  public name = 'KEENG';
+  public year = 2016;
+  //property binding
+  public image = "http://lorempixel.com/200/200";
+  public welcomeText = "Welcome KEENG";
+  public blueColor = true;
+}
--- quickstart-master/app/tutorial.component.ts
+++ quickstart-master/app/tutorial.component.ts
@@ -5,9 +5,37 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
-    template: '<h2>This is Keeng <small>vt</small>!!!!</h2>'
+    template: `<h2>This is Keeng <small>{{title}}</small>!!!!</h2>
+                <h3 [class.bg]="applyClass">This is develop</h3>
+                <button (click)="OnClick(name.value)">Click me</button>
+                <button (mouseover)="MouseOver($event)">MouseOver me</button>
+                <input type="text" #name>
+                <input type="text" value="two way binding" [(ngModel)]="fname" >
+                <input type="text" value="" [(ngModel)]="lname">
+                <br> {{fname}}  {{lname}}
+                
+            `,
+
+
+    styles: [`
+                h2 {color:red; font-style: italic}
+                .bg {background: silver}
+                
+            `]
 })
 
 export class TutorialComponent {
+    public applyClass = true;
+    public title = "VT - TT";
 
+    OnClick(value){
+        this.title = value;
+        alert('You have click ' + value);
+    }
+    MouseOver(e){
+        console.log(e);
+    }
+
+   // public fname = 1;
+    //public lname = 2;
 }
