42971ed2eba734e3abc3f5aea6884ad5dfd8e273
Fight Light Diamond <i.am.m.cuong@gmail.com>
2017-02-03 10:54:05 +0700

    update upload service

--- app/Services/UploadService.php
+++ app/Services/UploadService.php
@@ -6,68 +6,63 @@
  * Time: 10:12 PM
  */
 namespace App\Services;
+
 use App\Events\LogUploadEvent;
 use Intervention\Image\Facades\Image;
 
 class UploadService
 {
-    public function file($input, $path)
+    private $fileName = null;
+    private $basePath = null;
+    private $pathUploaded = null;
+
+    public function file($input, $basePath)
     {
+        $this->basePath = $basePath;
         $originalName = $input->getClientOriginalName();
-        $formatName = app('format')->formatNameFile($originalName);
-        $input->move($path, $formatName);
-        $path = $path .'/'. $formatName;
-        chmod($path, 0777);
-       // $path = str_replace(env('PREFIX_UPLOAD'), "", $path);
+        $this->fileName = app('format')->formatNameFile($originalName);
+        $input->move($basePath, $this->fileName);
+        $this->pathUploaded = $basePath . '/' . $this->fileName;
+        chmod($this->pathUploaded, 0777);
+        // $path = str_replace(env('PREFIX_UPLOAD'), "", $path);
         //event(new LogUploadEvent($path, "FILE"));
-        return $path;
+        return $this->pathUploaded;
     }
 
-    public function images($input, $path, $thumbImage)
+    public function images($input, $basePath, $thumbImage)
     {
-        $pathUploaded = $this->file($input, $path);
-        //$this->thumbImages($path, $pathUploaded, $formatName, $thumbImage);
-        //chmod($path, 0777);
-       // $path = str_replace(env('PREFIX_UPLOAD'), "", $path);
+        $this->file($input, $basePath);
+        $this->saveThumbs($thumbImage);
+        // $path = str_replace(env('PREFIX_UPLOAD'), "", $path);
         //event(new LogUploadEvent($path));
-        return $pathUploaded;
+        return $this->pathUploaded;
     }
 
-    private function thumbImages($path, $pathUploaded, $formatName, $thumbImage)
+    private function saveThumbs($thumbImage)
     {
-        $arrayThumb = explode('.', $formatName);
         foreach ($thumbImage as $folder => $sizes) {
             foreach ($sizes as $size) {
-                $this->processRender($path, $pathUploaded, $size, $arrayThumb, $folder);
+                $this->savingThumb($size, $folder);
             }
         }
     }
 
-    private function processRender($path, $pathUploaded, $size, $arrayThumb, $folder)
+    private function savingThumb($size, $folder)
     {
-        $pathThumb = $this->buildThumbPath($path, $size, $arrayThumb, $folder);
-        echo $pathThumb."<br>";
-        echo $size[0].'-'.$size[1].'<br>';
-        Image::make($pathUploaded)->resize($size[0], $size[1])->save($pathThumb);
-        chmod($pathThumb, 0777);
-        // event(new LogUploadEvent($pathThumb, 'THUMB', 0777));
+        $thumbPath = $this->getThumbPath($size, $folder);
+        Image::make($this->pathUploaded)->resize($size[0], $size[1])->save($thumbPath);
+        chmod($thumbPath, 0777);
+        // event(new LogUploadEvent($thumbPath, 'THUMB', 0777));
     }
 
-    private function buildThumbPath($path, $size, $arrayThumb, $folder)
+    private function getThumbPath($size, $folder)
     {
-        $sizer = '_' . implode('_', $size) . '.';
-        $formatName = implode($sizer, $arrayThumb);
-        if (!file_exists($path.$folder)) {
-            mkdir($path.$folder, 0777, true);
+        $sizeImage = '_' . implode('_', $size) . '.';
+        $fileName = str_replace('.', $sizeImage, $this->fileName);
+        $folderEnd = $this->basePath . $folder;
+        if (!file_exists($folderEnd)) {
+            mkdir($folderEnd, 0777, true);
         }
-        return $path.$folder. $formatName;
-    }
-
-    private function rendThumb($path, $formatName, $size, $folder)
-    {
-        $arrayThumb = explode('.', $formatName);
-        $pathThumb = $this->buildThumbPath($path, $size, $arrayThumb, $folder);
-        Image::make($path)->resize($size[0], $size[1])->save($pathThumb);
-        chmod($pathThumb, 0777);
+        return $folderEnd . $fileName;
     }
 }
